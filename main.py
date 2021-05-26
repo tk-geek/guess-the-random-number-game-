440f5df813c23d008b1dff0cae09d7d5064e7720
Tanvi Kachakayala <>
2021-05-26 23:54:30 +0000

    i added a print statement

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 import random
 
-number = random.randint (1 , 100)
+varNumber = random.randint (1 , 100)
 
 guess1 =  int( input("guess a number between 1 and 100 "))
 
-if (guess1 > number ):
+if (guess1 > varNumber ):
   print( "your guess was too high  guess again")
-elif(guess1 < number ) :
+elif(guess1 < varNumber ) :
   print(" your guess was too low guess again")
 else:
   print("congrats correct!!!!")
 
 
 
-guess2 =  int( input("guess a number between 1 and 10 "))
+guess2 =  int( input("guess a number between 1 and 100 "))
 
-if (guess2 > number ):
+if (guess2 > varNumber ):
   print( "your guess was too high  guess again")
-elif(guess2 < number ) :
+elif(guess2 < varNumber ) :
   print(" your guess was too low guess again")
 else:
   print("congrats correct!!!!")
 
 
-guess3 =  int( input("guess a number between 1 and 10 "))
+guess3 =  int( input("guess a number between 1 and 100 "))
 
-if (guess3 > number ):
+if (guess3 > varNumber ):
   print( "your guess was too high  guess again")
   print("Sorry!! you lost game dun dun dahh ")
-elif(guess3 < number ) :
+elif(guess3 < varNumber ) :
   print(" your guess was too low guess again")
   print("Sorry!! you lost the game dun dun dahhh")
 else:
   print("congrats correct!!!!")
  
+print("the correct number is ",varNumber )
